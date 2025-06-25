76bbf03f86384dabacac4f9f66d497c5a04644f5
Anuja201 <anujaherwade31@gmail.com>
2025-06-25 00:33:59 -0700

    demo3

--- Employee.java
+++ Employee.java
@@ -1,49 +1,5 @@
-package pratices;
-
-import java.util.Scanner;
+package pratices_demo3;
 
 public class Employee {
-	int empid;
-	String name;
-	double salary;
-	Date doj;
-	Vehicle veh;
-	
-	public Employee() {
-		System.out.println("Employee()");
-	}
-	public Employee(int empid, String name, double salary,int day ,int month,int year) {
-		this.empid = empid;
-		this.name = name;
-		this.salary = salary;
-		doj=new Date(day,month,year);
-	}
-	public void acceptEmployee(Scanner sc) {
-		System.out.print("Enter the employeeid:");
-		empid=sc.nextInt();
-		System.out.print("Enter the Name:");
-		name=sc.next();
-		System.out.print("Enter the Salary:");
-		salary=sc.nextDouble();
-		System.out.println("Enter the day of joining-");
-		doj.acceptDate(sc);
-	}
-	
-	public void addVehicle(Scanner sc) {
-		veh =new Vehicle();
-		veh.acceptVehicle(sc);
-		
-	}
-	
-	public void displayEmployee() {
-		System.out.println("empid - "+empid);
-		System.out.println("Name - "+name);
-		System.out.println("Salary - "+salary);
-		System.out.println("day of joining - ");
-		doj.displayDate();
-		if(veh != null)
-			veh.displayVehicle();
-	}
-	
 
 }
